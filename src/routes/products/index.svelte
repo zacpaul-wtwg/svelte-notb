a40f77523d57e91927f9bc93444532eb04b67f64
Rich Howell <rich@aurorasolar.com>
2022-04-28 14:56:27 -0400

    add fuzzy search example

--- src/routes/products/index.svelte
+++ src/routes/products/index.svelte
@@ -3,19 +3,36 @@
 </script>
 
 <script>
+	const filterProducts =
+		(strings) =>
+		({ title }) => {
+			const productTitleParts = title.toLowerCase().split(' ');
+			return strings.every((str) =>
+				productTitleParts.some((titlePart) => titlePart.startsWith(str))
+			);
+		};
+
 	export let products;
-	//console.log(products);
+	let searchString = '';
+	$: searchStrings = searchString.toLowerCase().split(' ');
+	$: filteredProducts = products.filter(filterProducts(searchStrings));
 </script>
 
-{#each products as product}
-	<div>
-		<a sveltekit:prefetch href={`/products/${product.id}`}>
-			<h2>{product.title}</h2>
-			<p>{product.description}</p>
-			<br />
-			<img src={product.imageThumb} alt="{product.title} IMAGE" />
-			<br />
-			<hr />
-		</a>
-	</div>
-{/each}
+<div>
+	<input type="text" bind:value={searchString} />
+	<div>Showing {filterProducts.length} of {products.length} results</div>
+</div>
+<div>
+	{#each filteredProducts as product}
+		<div>
+			<a sveltekit:prefetch href={`/products/${product.id}`}>
+				<h2>{product.title}</h2>
+				<p>{product.description}</p>
+				<br />
+				<img src={product.imageThumb} alt="{product.title} IMAGE" />
+				<br />
+				<hr />
+			</a>
+		</div>
+	{/each}
+</div>
