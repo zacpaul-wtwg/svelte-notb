88c4cbb3e08bc2c248bd53d7f0c25c07834e920b
ZacPaul <zac@websitesthatworkgood.com>
2022-05-22 19:11:14 -0400

    faq structured data

--- src/lib/faq.js
+++ src/lib/faq.js
@@ -2,20 +2,23 @@ import { sentenceify } from './utility/slugify';
 export const loadFaq = function (faqs) {
 	const newLocal = '<script type="application/ld+json">';
 	let faqsString = '';
+	let i = '0';
 	faqs.map((element) => {
 		faqsString =
 			faqsString +
-			`{
+			`${i === '0' ? '' : ','}{
                 "@type":"Question", 
                 "name":"${element.title}",
                     "acceptedAnswer": {
                         "@type": "Answer",
                         "text": "${sentenceify(element.answer)}"
                     }
-            },
+            }
         `;
+		i = i + 1;
 	});
 
+	faqsString.slice(0, -1);
 	const content = `
         "@context": "https://schema.org",
         "@type": "FAQPage",
