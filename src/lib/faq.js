82e0e4364ec1842569ace7cf405b5f934af87d24
ZacPaul <zac@websitesthatworkgood.com>
2022-05-22 17:41:19 -0400

    faq structured data

---
+++ src/lib/faq.js
@@ -0,0 +1,24 @@
+export const loadFaq = function (faqs) {
+	const newLocal = '<script type="application/ld+json">';
+	let faqsString = '';
+	faqs.map((element) => {
+		faqsString = `
+            ${faqsString}
+            "@type":"FAQPage", 
+            "name":"${element.title}",
+            "acceptedAnswer": {
+                "@type": "Answer",
+                "text": "${element.answer}"
+            },
+        `;
+	});
+
+	const content = `
+        "@context": "https://schema.org",
+        "@type": "FAQPage",
+        "mainEntity": [${faqsString}]
+    `;
+	const newLocal2 = '</script>';
+
+	return `${newLocal} {${content}} ${newLocal2}`;
+};
--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -16,6 +16,7 @@
 	import Accordion from '$lib/components/accordion.svelte';
 	import Container from '$lib/components/elements/Container.svelte';
 	import TitleBar from '$lib/components/TitleBar.svelte';
+	import { loadFaq } from '$lib/faq';
 	export let allData;
 </script>
 
@@ -23,6 +24,9 @@
 	title={'Frequently Asked Questions'}
 	description={'Frequently asked questions about the PA state laws, the safe use of fireworks, our pricing options, and more.'}
 />
+<svelte:head>
+	{@html loadFaq(allData.faq)}
+</svelte:head>
 <Container>
 	<section>
 		{#each allData.faq as entry}
