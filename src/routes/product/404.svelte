f43f4ca94815505debad2c1c35bb1d0db08b27bd
ZacPaul <zac@websitesthatworkgood.com>
2022-05-23 19:44:17 -0400

    fixed bug with redirects

---
+++ src/routes/product/404.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Container from '$lib/components/elements/Container.svelte';
+</script>
+
+<Container>
+	<h1>404 error</h1>
+	<h2>Failure to Launch</h2>
+	<p>
+		Looks like you followed a link to a product on our old site. Sorry! Just head over to our new
+		and improved <a href="/product">Products Page</a> to find what you're looking for!
+	</p>
+</Container>
