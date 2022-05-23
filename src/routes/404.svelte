cd31912f064ed5a96afdc4f8204940bba5137f7d
ZacPaul <zac@websitesthatworkgood.com>
2022-05-23 19:51:28 -0400

    custom 404 page

---
+++ src/routes/404.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Container from '$lib/components/elements/Container.svelte';
+</script>
+
+<Container>
+	<h1>404 error</h1>
+	<h2>What a dud!</h2>
+	<p>
+		This page is a dud. Fortunately, our fireworks work better than our website does (apparently).
+		Just head to our <a href="/">Home Page</a> and navigate around to find what you are looking for.
+	</p>
+</Container>
