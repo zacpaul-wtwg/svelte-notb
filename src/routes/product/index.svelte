981b8ccd330183fe4a8b98ad46338d70f9c52373
ZacPaul <zac@websitesthatworkgood.com>
2022-06-02 11:51:52 -0400

    button fix

--- src/routes/product/index.svelte
+++ src/routes/product/index.svelte
@@ -134,6 +134,25 @@
 </div>
 
 <style lang="scss">
+	button {
+		color: var(--white);
+		text-decoration: none;
+		padding: 0.5em 2em 0.5em 2em;
+		margin-top: 25px;
+		font-family: Langdon;
+		text-transform: uppercase;
+		font-size: 2em;
+		transition: 0.4s;
+		box-shadow: inset 0px 0px 0px 5px rgb(255, 255, 255);
+		background: var(--grey);
+		cursor: pointer;
+		display: block;
+	}
+	button:hover {
+		color: var(--white);
+		background-color: var(--grey);
+		box-shadow: none;
+	}
 	.search-params {
 		position: fixed;
 		top: var(--nav-height);
