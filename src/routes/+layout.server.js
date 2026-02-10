a0525b61bea0f0252d168726a08dfc7d1cd2dbd2
root <root@Zacs-Laptop.localdomain>
2026-02-10 07:36:39 -0500

    Serve CMS content at runtime via root layout

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,5 +1,7 @@
 import { loadCmsData } from '$lib/cms/loadCmsData';
 
+export const prerender = false;
+
 export async function load({ fetch }) {
 	const allData = await loadCmsData(fetch);
 	return { allData };
